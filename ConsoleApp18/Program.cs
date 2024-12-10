a07150517173699a1b1b130766c8539138550ecf
karamTahhan <kata07001@utb.vaxjo.se>
2024-12-10 13:45:48 +0100

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace For_loppar
+{
+    class program
+    {
+       static void Main(string[] args)
+        {
+            for (int i=10; i<=30;  i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int x=200; x>=180;  x--)
+            {
+                Console.Write(x);
+            }
+            for (int y=1000;  y<=1400; y+=50)
+            {
+                Console.WriteLine(y);
+            }
+
+        }
+
+
+
+    }
+
+
+
+
+}
